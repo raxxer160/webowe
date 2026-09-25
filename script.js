805cca0c8357fa75cb43809964d4a23a84d351d0
raxxer160 <konrad.polcyn@gmail.com>
2026-09-25 11:35:24 +0200

    MB4: dane jako tablica obiektow

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const umiejetnosci = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "SQL",
-  "PHP",
-  "Python",
-  "C++",
-  "C#"
+  { nazwa: "HTML", poziom: 4, kategoria: "frontend" },
+  { nazwa: "CSS", poziom: 3, kategoria: "frontend" },
+  { nazwa: "JavaScript", poziom: 3, kategoria: "frontend" },
+  { nazwa: "SQL", poziom: 2, kategoria: "backend" },
+  { nazwa: "PHP", poziom: 1, kategoria: "backend" },
+  { nazwa: "Python", poziom: 4, kategoria: "backend" },
+  { nazwa: "C++", poziom: 2, kategoria: "backend" },
+  { nazwa: "C#", poziom: 2, kategoria: "backend" }
 ];
 
 const formularz = document.querySelector("#formularz-kontakt");
